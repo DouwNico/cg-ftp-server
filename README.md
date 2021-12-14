300e5ce3fc7fd25dcdd0f9734bd9c1a82d190be1
DouwNico <douwe.nicolaas@gmail.com>
2021-12-14 16:59:31 +0100

    fix: script

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # cg-ftp-server
 a bash script for deploying a linux sftp server
+
+curl https://github.com/DouwNico/cg-ftp-server.git && chmod +x ./script.sh && ./script.sh
--- script.sh
+++ script.sh
@@ -1,10 +1,26 @@
 #!/bin/sh
 
-addgroup sftpgroup1
-useradd -m sftpuser1 -g sftpgroup1
-passwd sftpuser1 
-chmod 700 /home/sftpuser1/
+add_a_user() {
+    USER=$1
+    PASSWORD=$2
+    GROUP=$3
+    echo "Adding user $USER ..."
+    echo useradd -m $USER -g $GROUP
+    echo passwd $USER $PASSWORD
+    echo chmod 700 /home/$USER
+    echo "Added user $USER to $GROUP"
+}
 
-cp ./sshd_config /etc/ssh/sshd_config
-
-systemctl restart ssh
+###
+# Main body of script starts here
+###
+echo "Start of script..."
+echo addgroup sftpgroup1
+add_a_user sftpuser1 W@chtwoord sftpgroup1
+add_a_user douwe W@chtwoord sftpgroup1
+add_a_user pepijn W@chtwoord sftpgroup1
+echo "copy config to destination"
+echo cp --force ./sshd_config /etc/ssh/sshd_config
+echo "restarting ssh service"
+echo systemctl restart ssh
+echo "End of script..."
